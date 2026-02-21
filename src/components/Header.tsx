bfccf5712136e64463e0b07125b6ac6ca033d9ae
Ryan McKinstry <52261222+Rmckinstry@users.noreply.github.com>
2026-02-20 20:03:54 -0600

    updating name refs.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ function Header() {
         <img id="header-img" src={HeaderImage} alt={"Green Vines"} />
         <div id="header-info-container" className="flex-col">
           <h1 id="header-name" className="contain-text-center">
-            BAILEY & RYAN
+            Shelby & TYLER
           </h1>
           <p id="header-event-info-desktop" className="font-med-lg">
             November 15, 2025, 3:00 PM • Rossville, TN
