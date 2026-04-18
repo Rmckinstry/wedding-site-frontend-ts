ef8af0ccccdd5a0ecc07ceee66069587c965d7a0
Ryan McKinstry <52261222+Rmckinstry@users.noreply.github.com>
2026-04-17 22:59:50 -0500

    changing title

--- src/components/Registry.tsx
+++ src/components/Registry.tsx
@@ -7,7 +7,7 @@ function Registry() {
         <div id="registry-info-container" className="flex-col"></div>
         <div id="registry-btn-container" className="btn-container contain-text-center">
           <iframe
-            title="registry-iframe"
+            title="Registry Preview"
             src="https://withjoy.com/tyler-and-shelby-sep-26/registry"
             // onClick={() => window.open("https://withjoy.com/tyler-and-shelby-sep-26/registry", "_blank")}
             className="registry-frame"
